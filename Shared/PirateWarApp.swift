b7dd9c901989494b5491514c37aef6df3f267c34
Minh Pham <pcminh0505@gmail.com>
2022-08-09 21:12:52 +0700

    Initial Commit

---
+++ Shared/PirateWarApp.swift
@@ -0,0 +1,17 @@
+//
+//  PirateWarApp.swift
+//  Shared
+//
+//  Created by Minh Pham on 09/08/2022.
+//
+
+import SwiftUI
+
+@main
+struct PirateWarApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
