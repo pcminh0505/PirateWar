09ccbca7fc23cc9a3498227be40e56fd905f7836
Minh Pham <pcminh0505@gmail.com>
2022-08-29 20:49:59 +0700

    Update PirateWarApp.swift

--- Shared/PirateWarApp.swift
+++ Shared/PirateWarApp.swift
@@ -1,15 +1,27 @@
-//
-//  PirateWarApp.swift
-//  Shared
-//
-//  Created by Minh Pham on 09/08/2022.
-//
+/*
+    RMIT University Vietnam
+    Course: COSC2659 iOS Development
+    Semester: 2022B
+    Assessment: Assignment 2
+    Author: Pham Cong Minh
+    ID: s3818102
+    Created  date: 09/08/2022
+    Last modified: 29/08/2022
+    Acknowledgement: Mobiraft (https://mobiraft.com/ios/swiftui/how-to-add-splash-screen-in-swiftui/)
+*/
 
 import SwiftUI
 
 @main
 struct PirateWarApp: App {
     @StateObject var users = Users()
+    
+    init() {
+        UserDefaults.standard.register(defaults: [
+            "backgroundMusic": true,
+            "soundEffect": true,
+        ])
+    }
     var body: some Scene {
         WindowGroup {
             LaunchView()
